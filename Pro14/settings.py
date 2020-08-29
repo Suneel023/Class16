601af2c132d21c0fccab9c09837e1723036c85c3
Suneel023 <mariochill@yahoo.com>
2020-08-30 03:35:47 +0530

    'BFormsCrispy'

--- Pro14/settings.py
+++ Pro14/settings.py
@@ -44,10 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp', #appname
-    
+    'crispy_forms',
 ]
 
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- myapp/views.py
+++ myapp/views.py
@@ -9,6 +9,28 @@ from myapp import forms
 
 def builtinforms(request):
 
+    if request.method=="POST":
+        
+        # Creating a form instance with the data filled in all fields will get the specified value
+        form=forms.SampleForm(request.POST)
+        
+        if form.is_valid():
+        
+            # cleaned_data is variable in form instance that holds the dictionary containing the data 
+            # that we have filled 
+            first_name=form.cleaned_data.get('first_name')
+            last_name=form.cleaned_data.get('last_name')
+            email=form.cleaned_data.get('email')
+            phno=form.cleaned_data.get('phno')
+            pwd=form.cleaned_data.get('pwd')
+            birth_day=form.cleaned_data.get('birth_day')
+            birth_month=form.cleaned_data.get('birth_month')
+            birth_year=form.cleaned_data.get('birth_year')
+            gender=form.cleaned_data.get('gender')
+            data=form.cleaned_data
+            return render(request,"displaydata.html",context=data)
+
+
     form=forms.SampleForm()
     return render(request,"builtinform.html",{'form':form})
 
